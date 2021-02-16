d64623bcaa71fbf0b90bdc4e0887ab15ca8a95d3
daan166 <daan.edelkoort@gmail.com>
2021-02-16 23:07:51 +0100

    compare mechanism

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-//import { StatusBar } from 'expo-status-bar';
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import { Button, StyleSheet, Text, View, Image, Dimensions} from 'react-native';
-//import * as Permissions from 'expo-permissions'
-//import * as ImagePicker from 'expo-image-picker'
-//import * as Vibrant from 'node-vibrant'
-//import {getColors} from 'react-native-color-grabber'
-//import Palette from 'react-native-palette-full'
 import {getAllSwatches} from 'react-native-palette'
 import {request, check, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import ImagePicker from 'react-native-image-crop-picker';
@@ -18,6 +12,7 @@ class App extends Component {
       buttonPressed: false,
       hasCameraPermission: null,
       image: null,
+      imageSelection: null
     }
   }
 
@@ -36,43 +31,21 @@ class App extends Component {
   
 
   _getPhotoLibrary = async () => {
-    ImagePicker.openPicker({
-      multiple: true, includeExif: true
-    }).then(images => {
-      console.log(images);
-      urlFromPath = images[0].path.substring('file://'.length)
-      console.log(urlFromPath)
-      this.setState({ image: images[0].path })
-      getAllSwatches({}, urlFromPath, (error, swatches) => {
-        //console.log(swatches, error)
-        swatches.sort((a, b) => {
-          return b.population - a.population;
-        });
-        swatches.forEach((swatch) => {
-          console.log(swatch.swatchInfo);
-        });
-      
-      
-      })
-      //Palette.getAllSwatches(images[0].path).then(outputColors => {console.log(outputColors)});
-    });
-    // launchImageLibrary({}, response => {
-    //   this.setState({ image: response.uri })
-    //   console.log(response)
-    // })
-    // let result = await ImagePicker.launchImageLibraryAsync({
-    //   allowsMultipleSelection: true,
-    //   exif: true
-    // });
-    // if (!result.cancelled) {
-    //   this.setState({ image: result.uri });
-    //  console.log(result)
-    // getAllSwatches(options, result.uri, (error, swatches) => {console.log(swatches)})
-    //getAllSwatchesFromUrl(result.uri).then(outputColors);  
-    //Vibrant.from(result.uri).getPalette().then((palette) => console.log(palette))
-    //getColors(result.uri, (err, res) => {console.log(res)})
-    //}
-   }
+    let images = await ImagePicker.openPicker({multiple: true, includeExif: true})
+    //console.log(images)
+    
+    let imageSelection = []
+    for (let i = 0; i < images.length; i++) {
+      imageSelection.push({
+        path: images[i].path, 
+        dateTime: images[i].exif.DateTime, 
+        swatches: JSON.stringify(await returnSwatches(images[i].path.substring('file://'.length)))
+      })  
+    }
+    //console.log(imageSelection)
+    imageCompare(imageSelection)
+    this.setState({ image: imageSelection[0].path })
+  }
 
   render(){
     return (
@@ -97,14 +70,9 @@ class App extends Component {
             <Image source={{uri: this.state.image}} style={{ flex: 1 }}/>
           ):(
             <Text>No image</Text>
-
           )
-
-
-
         }
         </View>
-
       </View>
     );
   }
@@ -127,5 +95,84 @@ const styles = StyleSheet.create({
     borderColor: "#fff"
    },
 })
+
+const returnSwatches = (path) => {
+  return new Promise(resolve => {
+    getAllSwatches({}, path, (error, swatches) => {
+      swatches.sort((a, b) => {
+        return b.population - a.population;
+      });
+      swatches = swatches.slice(0,5) //top x swatches on population will be included
+      resolve(swatches)
+    })
+  })
+}
+
+const imageCompare = async (imageSelection) => {
+  let images = {}
+  for (let i_images = 0; i_images < imageSelection.length; i_images++) {
+    
+    //console.log(imageSelection[i_images])
+    let swatches = JSON.parse(imageSelection[i_images].swatches)
+    let swatchesTemp = []
+    for (let i_swatches = 0; i_swatches < swatches.length; i_swatches++) {
+      
+      let swatchColor = swatches[i_swatches].color
+      let swatchColorArray = swatchColor.substring('rgba('.length).split(',')
+      let swatchColorRGB = {R: swatchColorArray[0], G: swatchColorArray[1], B: swatchColorArray[2]}
+      swatchesTemp.push(swatchColorRGB)
+      //console.log(swatchColorRGB)
+
+    }
+    images[imageSelection[i_images].path] = swatchesTemp
+  }
+  let images_colordistance = {}
+
+  for (let image_base in images){
+    let image_base_swatches = images[image_base]
+    let image_colordistance = {}
+
+    for (let image_compare in images){
+      let image_compare_swatches = images[image_compare]
+      let swatches_colordistance = []
+
+      for (let image_base_swatch_index in image_base_swatches){
+        let image_base_swatch = image_base_swatches[image_base_swatch_index]
+        let swatch_colordistance = []
+
+        for (let image_compare_swatch_index in image_compare_swatches){
+          let image_compare_swatch = image_compare_swatches[image_compare_swatch_index]
+          let colordistance = 
+            // Math.sqrt(
+            //   Math.pow((image_base_swatch.R - image_compare_swatch.R), 2) +   //R
+            //   Math.pow((image_base_swatch.G - image_compare_swatch.G), 2) +   //G
+            //   Math.pow((image_base_swatch.B - image_compare_swatch.B), 2)     //B
+            // )
+            (image_base_swatch.R - image_compare_swatch.R) +   //R
+            (image_base_swatch.G - image_compare_swatch.G) +   //G
+            (image_base_swatch.B - image_compare_swatch.B)     //B
+          swatch_colordistance.push(colordistance)
+        }
+        swatches_colordistance.push(sumArray(swatch_colordistance))
+      }
+      //image_colordistance[image_compare] = JSON.stringify(swatches_colordistance)
+      image_colordistance[image_compare] = Math.sqrt(Math.pow(sumArray(swatches_colordistance),2))
+
+    }
+    images_colordistance[image_base] = image_colordistance
+  }
+  console.log(images_colordistance)
+
+
+
+}
+
+
+const sumArray = (array) => {
+  let sum = array.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  return sum;
+}
 
 export default App
